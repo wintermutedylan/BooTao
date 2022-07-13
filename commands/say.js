445256a84d1b4c01a924044f2ab16ff6518a34df
Dylan Wintermute <dylan.wintermute.17@cnu.edu>
2022-07-13 11:00:28 -0400

    udpated say command to use images as well.  only the 1 at a time

--- commands/say.js
+++ commands/say.js
@@ -12,8 +12,14 @@ module.exports = {
         var guideRole = "831221217364017202";
         var ID = message.author.id;
         var target = message.guild.members.cache.get(ID);
+        var hasAttachment = false;
+        if (message.attachments.size > 0){
+            var file = message.attachments.at(0);
+            hasAttachment = true;
+        }
         var channel = args[0];
         var response = args.slice(1).join(" ");
+        
         
         if (target.roles.cache.some(role => role.id === modRole || role.id === guideRole)){
 
@@ -21,9 +27,17 @@ module.exports = {
             channel = channel.slice(2, -1);
         }
         try {
-            client.channels.cache.get(channel).send(response);
+            if (hasAttachment && !response === ''){
+                client.channels.cache.get(channel).send({ content: response, files: [file]});
+            } 
+            else if(hasAttachment){
+                client.channels.cache.get(channel).send({ files: [file]});
+            } else {
+                client.channels.cache.get(channel).send(response);
+            }
         } catch(err){
             message.reply("There was an error using this command, make sure to mention a channel");
+            console.log(err);
             
             client.channels.cache.get("838666046327619604").send(codeBlock('js', err));
         }
