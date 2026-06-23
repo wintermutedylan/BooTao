09a4a4be99c4833ae516515ebf4605cdfc87674e
wintermutedylan <wintermutedylan@gmail.com>
2026-06-23 10:11:53 -0400

    new coin flip command

---
+++ commands/coinflip.js
@@ -0,0 +1,24 @@
+const { userMention } = require("@discordjs/builders");
+const custom = require("../models/customCommands");
+
+module.exports = {
+    name: 'coinflip',
+    aliases: [],
+    permissions: [],
+    description: "Create user profile",
+    async execute(client, message, cmd, args, Discord){
+        
+
+        const result = Math.random() < 0.5 ? "Heads" : "Tails";
+
+        message.channel.send(result);
+
+
+
+        
+        
+
+
+
+    }
+}
